8bf489de89237dc8275d5289fc4d2cd61e77006b
chuan1983 <chuan1983126@gmail.com>
2016-09-22 10:16:31 +0800

    v8

--- app/src/main/java/org/iii/tw/mylistv2/List2Activity.java
+++ app/src/main/java/org/iii/tw/mylistv2/List2Activity.java
@@ -2,7 +2,11 @@ package org.iii.tw.mylistv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.LinkedList;
 
@@ -11,6 +15,7 @@ public class List2Activity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private String[] dataset = {"item1","item2","item3","item4","item5","item6"};
     private LinkedList<String>data;
+    private LinearLayoutManager mLayoutManager;
 
 
     @Override
@@ -24,5 +29,41 @@ public class List2Activity extends AppCompatActivity {
         }
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclervuew);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+//        // specify an adapter (see also next example)
+//        mAdapter = new MyAdapter(myDataset);
+//        recyclerView.setAdapter(mAdapter);
     }
+    private class MyAdapter extends RecyclerView.Adapter{
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView mTextView;
+            public MyViewHolder(View v) {
+                super(v);
+                mTextView = (TextView) v;
+            }
+        }
+        public MyAdapter() {
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
 }
